7333a4d21bcfc7c7630006822bf3e10c7289758f
mgrddsj <Jesse_Xu@live.com>
2019-09-12 09:03:18 +0800

    Today's work. Need to mess with MediaPlayer.

--- app/src/main/java/com/example/menu/MusicPlayer.java
+++ app/src/main/java/com/example/menu/MusicPlayer.java
@@ -2,14 +2,18 @@ package com.example.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MusicPlayer extends AppCompatActivity {
+MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
         setTitle("Music Player");
+
+        player = MediaPlayer.create(getApplicationContext(), R.raw.amesfluid);
     }
 }
